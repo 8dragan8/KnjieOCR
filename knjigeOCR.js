8e6a0ccbb4849cdb9a5c3234ba6a3cc2308b074d
dre <bata.raka.inc@gmail.com>
2018-11-08 13:34:25 +0100

    Dodata klasa knjige i preuredjena za bolje raspored foldera

--- knjigeOCR.js
+++ knjigeOCR.js
@@ -4,6 +4,23 @@ const path = require('path')
 // const cheerio = require('cheerio')
 const fs = require('fs')
 const URL = require('url').URL
+const ROOTDIR = 'Knjige/'
+
+
+class knjige {
+	constructor(autor, naslov, link, vrsta) {
+		this.autor = autor
+		this.naslov = naslov
+		this.link = new URL(link)
+		this.vrsta = vrsta
+	}
+}
+let listKnjige = []
+listKnjige[0] = new knjige('Vasko Popa', 'Pesme', 'http://digitalna.nb.rs/wb/NBS/Knjige/Srpska_knjizevnost_u_100knjiga/II-153244-098?pageIndex=00001', 'poezija')
+listKnjige[1] = new knjige('Antonije Isaković', 'Pripovedač', 'http://digitalna.nb.rs/wb/NBS/Knjige/Srpska_knjizevnost_u_100knjiga/II-153244-097?pageIndex=00005', 'proza')
+
+console.log(listKnjige)
+
 
 
 // let path = require('path')
@@ -15,27 +32,40 @@ const URL = require('url').URL
 // console.log('URL query - ' + img_url.query)
 
 
-loopKnjige()
+loopStrane(listKnjige[0])
 
 
-async function loopKnjige() {
+async function loopStrane(knjiga) {
 	let i = 1
 	let kraj = true
+	let img_url = knjiga.link
+	let dirname = ROOTDIR + knjiga.autor + '/' + knjiga.naslov + '/'
+
+	fs.mkdir(dirname, {
+		recursive: true
+	}, (err) => {
+		if (err) throw err
+	})
+
 	while (kraj) {
-		let img_url = new URL('http://digitalna.nb.rs/wb/NBS/Knjige/Srpska_knjizevnost_u_100knjiga/II-153244-097?pageIndex=00005')
-		let query = '0000' + i.toString()
+
+		let query = ('0000' + i.toString()).slice(-5)
 		// console.log(img_url)
-		img_url.searchParams.set('pageIndex', query.slice(-5))
+
+		img_url.searchParams.set('pageIndex', query)
 		// console.log('URL href - ' + img_url.href)
-		await KnjigeOCR(img_url).then(function () {
-			i++
-		}).catch(function () {
-			kraj = false
-		})
+
+		await downloadStrana(img_url, dirname)
+			.then(function () {
+				i++
+			})
+			.catch(function () {
+				kraj = false
+			})
 	}
 }
 
-function KnjigeOCR(img_url) {
+function downloadStrana(img_url, dirname) {
 	return new Promise(function (resolve, reject) {
 
 
@@ -45,11 +75,14 @@ function KnjigeOCR(img_url) {
 
 			if (!error && response.statusCode == 200) {
 
-				let img_name = path.parse(img_url.pathname.slice(-13) + '/' + img_url.searchParams.get('pageIndex') + '.jpg')
 
-				request(img_url.href).on('error', function (err) {
-					console.log(err)
-				}).pipe(fs.createWriteStream(path.format(img_name)))
+				let img_name = path.parse(dirname + img_url.searchParams.get('pageIndex') + '.jpg')
+
+				request(img_url.href)
+					.on('error', function (err) {
+						console.log(err)
+					})
+					.pipe(fs.createWriteStream(path.format(img_name)))
 
 				// OCRocitavanje(path.format(img_name))
 
@@ -61,6 +94,3 @@ function KnjigeOCR(img_url) {
 		})
 	})
 }
-
-
-
